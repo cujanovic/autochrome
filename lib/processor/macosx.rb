edb2e34984ae7091ed98307dfa5dbca85935451c
Predrag Cujanovic <predrag@cujanovic.com>
2020-05-02 22:19:00 +0200

    update

--- lib/processor/macosx.rb
+++ lib/processor/macosx.rb
@@ -49,7 +49,6 @@ ENV["GOOGLE_DEFAULT_CLIENT_ID"]="invalid"
 ENV["GOOGLE_DEFAULT_CLIENT_SECRET"]="invalid"
 
 opts = [
-  "--ignore-certificate-errors",
   "--disable-xss-auditor",
   "--no-default-browser-check",
   "--no-first-run",
